b3b1b4d426d954b89dcaef1b18f4b8a4071245c8
kidchenko <kidchenko@gmail.com>
2017-04-27 03:56:00 -0300

    Remove Mongo URI

--- bot.js
+++ bot.js
@@ -1,3 +1,20 @@
 const botBuilder = require('claudia-bot-builder');
+const fbTemplate = botBuilder.fbTemplate;
+const MongoClient = require('mongodb').MongoClient;
 
-module.exports = botBuilder(request => `Hello from dr bot. Your request was ${request.text}`);
+const mongoUri = process.env.MONGO_URI;
+
+module.exports = botBuilder(request => {
+    
+    MongoClient.connect(mongoUri, { uri_decode_auth: true }, function(err, db) {
+
+    var collection = db.collection('estabSus');
+    collection.findOne(function(err, one) {
+        return new fbTemplate.Text(one.geoJson.name).addQuickReplyLocation().get()
+    });
+    
+    db.close();
+
+    });
+    
+});
